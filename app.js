6eef00f91dd316d2e92ef56bc788a5f2f8086cb6
lsh3384 <lsh3384@gmail.com>
2022-01-03 23:13:39 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res)=>{
+	res.sendFile(path.join(__dirname, './index.html'));
+})
+
+const HTTPServer = app.listen(30001, ()=>{
+	console.log("Server is open at port:30001");
+});
+
+
+const wsModule = require('ws');
+
+const webSocketServer = new wsModule.Server(
+	{
+		server: HTTPServer
+	}
+);
+
+
+webSocketServer.on('connection', (ws, request)=>{
+	const ip = request.headers['x-forwareded-for'] || request.connection.remoteAddress;
+	
+	console.log('new client[&{ip}] welcome from server');
+	
+	ws.on('message', (msg)=>{
+		console.log('message from client[${ip}] is : ${msg}');
+		ws.send('message received from server');
+	})
+	
+	ws.on('error', (error)=>{
+		console.log('client[${ip}] connection error occured : ${error}');
+	})
+	
+	ws.on('close', ()=>{
+		console.log('client[%{ip}] web socket closed');	
+	})
+	
+});
